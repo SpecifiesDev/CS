8d73d0b29dc47203a7374f5a660ee7f7d4d022b5
Austin <38254442+SpecifiesDev@users.noreply.github.com>
2018-12-07 14:51:06 -0500

    Add files via upload

--- create task/core.js
+++ create task/core.js
@@ -8,6 +8,9 @@ var ctx = canvas.getContext("2d");
 // snake block pieces, first block in this iss counted as a head
 var blocks = [];
 
+// food pieces
+var food = [];
+
 //counter 
 var seconds = 0;
 var start = Date.now();
@@ -16,10 +19,20 @@ var start = Date.now();
 var grid = [];
 var w;
 var h;
+var rows;
+var columns;
 
 function setup() {
 	buffer();
 	setGrid();
+	log(grid);
+	log(rows);
+	generateRandom();
+}
+
+function draw() {
+	ctx.clearRect(0, 0, canvas.width, canvas.height);
+
 }
 
 function buffer() {
@@ -51,15 +64,55 @@ function buffer() {
 function setGrid() {
 	// 2d arrays
 	rows = w / 5;
+
+	// we define a colum object to only go (column) down the 2d array
+	columns = h / 5;
 	log("Creating game grid with " + rows + " rows.");
 	for(var i = 0; i < rows; i++) {
 		grid[i] = [];
 	}
 	for(var i = 0; i < rows; i++) {
-		grid[i][i] = 1; 
+		grid[i][i] = 1;
 	}
 
 
 }
+function generateRandom() {
+	log("random");
+	log(grid[50][50]);
+	// first we select a random place on our grid.
+	var x = Math.floor((Math.random() * rows) + 1);
+	var y = Math.floor((Math.random() * columns) + 1);
+	log(x);
+	var data = grid[x][y];
+	log(data);
+	if(data == 0) {
+		generateRandom();
+	}
+	if(data == 1) {
+		var mx = x * 5;
+		var my = y * 5;
 
+		var togen = rectangle({x: mx, y: my, color: "blue"})
+		togen.draw();
+
+		grid[x][y] = 0;
+		log("Generate new rect at: " + mx + " - " + my);
+	}
+
+}
+function addBlock() {
+
+}
+function rectangle(r) {
+	
+	r.draw = function() {
+		ctx.beginPath();
+		ctx.fillStyle = color;
+		ctx.rect(x, y, wid, hei);
+		ctx.fill();
+		ctx.closePath();
+	}
+	return r;
+}
 setup();
